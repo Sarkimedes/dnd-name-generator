f2a9a2ac5fc9b280705a78e1e81831e7248a4f2a
Peter Roberts <peter@blackmarble.com>
2016-02-25 22:02:11 +0000

    added styling

--- app/controllers/names-controller.js
+++ app/controllers/names-controller.js
@@ -1,38 +1,76 @@
 (function()  {
-    var app = angular.module('chedMakerApp', [])
-    app.controller('NamesController', ['$scope', function ($scope) {
-        var names = {
-            beginnings: [
-                '', '', '', '', 'a', 'be', 'de', 'el', 'fa', 'jo',
-                'ki', 'la', 'ma', 'na', 'o', 'pa', 're', 'si', 'ta', 'va'
-            ],
-            middles: [
-                'bar', 'ched', 'dell', 'far', 'gran', 'hal', 'jen', 'kel',
-                'lim', 'mor', 'net', 'penn', 'quil', 'rond', 'sark', 'shen',
-                'tur', 'vash', 'yor', 'zen'
-            ],
-            ends: [ 
-                '', 'a', 'ac', 'ai', 'al', 'am', 'an', 'ar', 'ea', 'el', 
-                'er', 'ess', 'ett', 'ic', 'id', 'il', 'in', 'is', 'or', 'us' 
-            ]
-        }
-        var generateName = (function() {
-            var getRandomIndex = (function () {
-                return Math.floor(Math.random() * 20);
-            });
-            var beginningIndex = getRandomIndex();
-            var middleIndex = getRandomIndex();
-            var endIndex = getRandomIndex();
-            var name = 
-                (names.beginnings[beginningIndex] + 
-                names.middles[middleIndex] +
-                names.ends[endIndex]);
-             return name.charAt(0).toUpperCase() + name.slice(1);
-        });
-        $scope.name = generateName();
-        
+    angular.
+    module('chedMakerApp', []).
+    controller('NamesController', [
+        '$scope', 
+        'names', 
+        'generateRace', 
+        'generateGender',
+        function ($scope, names, generateRace, generateGender) {
         $scope.generate = function () {
-            $scope.name = generateName();
+            $scope.name = names.generateName();
+            $scope.race = generateRace();
+            $scope.gender = generateGender();
         }
-    }]);
+    }]).
+    factory('names', function () {
+        return (function () {
+            var names = {
+                beginnings: [
+                    '', '', '', '', 'a', 'be', 'de', 'el', 'fa', 'jo',
+                    'ki', 'la', 'ma', 'na', 'o', 'pa', 're', 'si', 'ta', 'va'
+                ],
+                middles: [
+                    'bar', 'ched', 'dell', 'far', 'gran', 'hal', 'jen', 'kel',
+                    'lim', 'mor', 'net', 'penn', 'quil', 'rond', 'sark', 'shen',
+                    'tur', 'vash', 'yor', 'zen'
+                ],
+                ends: [ 
+                    '', 'a', 'ac', 'ai', 'al', 'am', 'an', 'ar', 'ea', 'el', 
+                    'er', 'ess', 'ett', 'ic', 'id', 'il', 'in', 'is', 'or', 'us' 
+                ]
+            }
+            var generateName = (function() {
+                var getRandomIndex = (function () {
+                    return Math.floor(Math.random() * 20);
+                });
+                var beginningIndex = getRandomIndex();
+                var middleIndex = getRandomIndex();
+                var endIndex = getRandomIndex();
+                var name = 
+                    (names.beginnings[beginningIndex] + 
+                    names.middles[middleIndex] +
+                    names.ends[endIndex]);
+                return name.charAt(0).toUpperCase() + name.slice(1);
+            });
+            return {
+                generateName : generateName
+            }
+        })();
+    }).
+    factory('generateRace', function() {
+        return (function () {
+            var races = [
+                'Dragonborn',
+                'Drow',
+                'Dwarf',
+                'Elf',
+                'Gnome',
+                'Half-elf',
+                'Half-orc',
+                'Halfling',
+                'Human',
+                'Tiefling'
+            ]
+            return function() {
+                return races[Math.floor(Math.random() * races.length)]
+            }
+        })();
+    }).
+    factory('generateGender', function() {
+        return function() {
+            var gender = Math.random() + 0.5 >= 1 ? 'Male' : 'Female';
+            return gender;
+        } 
+    });
 })();
